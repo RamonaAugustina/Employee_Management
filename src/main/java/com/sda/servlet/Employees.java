9d94c5e895a5907d5f7590b2b7f13b3ef44940bc
Soldan Diana-Maria <you@example.com>
2019-09-19 23:43:48 +0300

    Created form for employee registration

--- src/main/java/com/sda/servlet/Employees.java
+++ src/main/java/com/sda/servlet/Employees.java
@@ -2,6 +2,7 @@ package com.sda.servlet;
 
 import com.sda.dao.EmployeeDao;
 import com.sda.model.Employee;
+import com.sda.service.EmployeeService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,11 +15,25 @@ import java.io.IOException;
 public class Employees extends HttpServlet {
 
     private EmployeeDao employeeDao = new EmployeeDao();
+    private EmployeeService employeeService = new EmployeeService();
 
     @Override
     protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
             throws ServletException, IOException {
         Employee employee = employeeDao.getEntityById(Employee.class, 1L);
         response.getWriter().println("Hello World!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String firstName = req.getParameter("first_name");
+       String lastName = req.getParameter("last_name");
+
+       Employee emp = employeeService.createEmployee(firstName,lastName,0,"departament");
+       employeeDao.createEntity(emp);
+
+      //TO DO: Create employee in database
+
+        resp.sendRedirect("employees.jsp");
     }
 }
--- src/main/webapp/employees.jsp
+++ src/main/webapp/employees.jsp
@@ -55,5 +55,18 @@
             </tr>
         </c:forEach>
     </table>
+
+<form action="employees" method="post">
+    <table style="with: 50%">
+        <tr>
+            <td>First Name</td>
+            <td><input type="text" name="first_name" /></td>
+        </tr>
+        <tr>
+            <td>Last Name</td>
+            <td><input type="text" name="last_name" /></td>
+        </tr>
+       </table>
+    <input type="submit" value="Submit" /></form>
 </body>
 </html>
